22d8c9ec8ed10c2c6d31e15a717e5dd08dd4754f
Sherry <sherryshiyiwang@gmail.com>
2019-10-04 13:47:50 -0700

    save

---
+++ Assets/Scripts/PressureMeter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PressureMeter : MonoBehaviour
+{
+    private const float maxPressureAngle = -20;
+    private const float minPressureAngle = 210;
+
+    private Transform needleTranform;
+    //private Transform speedLabelTemplateTransform;
+
+    private float pressureMax;
+    private float pressure;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        needleTranform = transform.Find("needle");
+        //speedLabelTemplateTransform = transform.Find("speedLabelTemplate");
+        //speedLabelTemplateTransform.gameObject.SetActive(false);
+
+        pressure = 0f;
+        pressureMax = 200f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePlayerInput();
+
+        //pressure += 30f * Time.deltaTime;
+        //if (pressure > pressureMax) pressure = pressureMax;
+
+        needleTranform.eulerAngles = new Vector3(0, 0, GetPressureRotation());
+    }
+
+
+    private float GetPressureRotation()
+    {
+        float totalAngleSize = minPressureAngle - maxPressureAngle;
+
+        float pressureNormalized = pressure / pressureMax;
+
+        return minPressureAngle - pressureNormalized * totalAngleSize;
+    }
+
+    private void HandlePlayerInput() {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            float deceleration = 50f;
+            pressure -= deceleration * Time.deltaTime;
+        }
+        else {
+            float acceleration = 20f;
+            pressure += acceleration * Time.deltaTime;
+        }
+
+        pressure = Mathf.Clamp(pressure, 0f, pressureMax); // to make sure it's in the right range
+
+    }
+
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Text text;
+    float theTime; // keeps a track of the current time
+    public float speed = 1; //by seconds, mins, or hrs
+    bool counting;
+    bool reset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartTimer();
+        StopTimer();
+        ResetTimer();
+
+        if (counting == true)
+        {
+            theTime += Time.deltaTime * speed;
+            string hours = Mathf.Floor((theTime % 216000) / 3600).ToString("00"); // 3600 * 60
+            string minutes = Mathf.Floor((theTime % 3600) / 60).ToString("00"); // 60 * 60; floor is to round the value
+            string seconds = (theTime % 60).ToString("00");
+            text.text = hours + ":" + minutes + ":" + seconds;
+        }
+
+        if (reset == true) {
+            text.text = "00:00:00";
+            theTime = 0;
+        }
+    }
+
+    public void StartTimer() {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            counting = true;
+        }
+    }
+
+    public void StopTimer()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            counting = false;
+        }
+    }
+
+    public void ResetTimer()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            reset = true;
+        }
+        else {
+            reset = false;
+        }
+    }
+}
